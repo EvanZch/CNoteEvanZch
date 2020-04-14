b66a0bd326a29319ab2f33200ed8e973fd889ab9
EvanZch <zou0212>
2020-04-14 10:41:25 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/CNoteEvanZch.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CNoteEvanZch.dir/main.c.obj"
+  "CNoteEvanZch.pdb"
+  "CNoteEvanZch.exe"
+  "CNoteEvanZch.exe.manifest"
+  "libCNoteEvanZch.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CNoteEvanZch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CNoteEvanZch.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CNoteEvanZch.
+# This may be replaced when dependencies are built.
